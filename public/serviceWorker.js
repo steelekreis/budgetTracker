d709a291532d056c0cfeb5b688e17dfa566ebcd9
steelekreis <steele.kreis@gmail.com>
2022-02-26 23:59:42 -0700

    fixing refs in manifest and serviceWorker. also adding a few things to the cache in serviceWorker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,10 +1,13 @@
 const FILES_TO_CACHE = [
     "/",
+    "manifest.webmanifest",
     "index.html",
-    "styles.css",
-    "index.js",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512.png",
+    "assets/css/styles.css",
+    "assets/js/index.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "assets/js/db.js",
+    "assets/images/icons/icon-192x192.png",
+    "assets/images/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
